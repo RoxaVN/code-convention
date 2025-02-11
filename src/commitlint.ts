2ac48b016e2cdd59f21fc28bc2201ac460758a58
Toan Nguyen <woodpecker146@gmail.com>
2025-02-11 20:51:24 +0700

    fix: read wrong commit message

--- src/commitlint.ts
+++ src/commitlint.ts
@@ -1,57 +1,60 @@
-import lint from "@commitlint/lint";
+#!/usr/bin/env node
+
+import fs from 'node:fs';
+import lint from '@commitlint/lint';
 import {
-	RuleConfigSeverity,
-	type RuleConfigCondition,
-	type TargetCaseType,
-} from "@commitlint/types";
+  type RuleConfigCondition,
+  RuleConfigSeverity,
+  type TargetCaseType,
+} from '@commitlint/types';
 
 async function main() {
-	const result = await lint(process.argv[2], {
-		"body-leading-blank": [RuleConfigSeverity.Warning, "always"] as const,
-		"body-max-line-length": [RuleConfigSeverity.Error, "always", 100] as const,
-		"footer-leading-blank": [RuleConfigSeverity.Warning, "always"] as const,
-		"footer-max-line-length": [
-			RuleConfigSeverity.Error,
-			"always",
-			100,
-		] as const,
-		"header-max-length": [RuleConfigSeverity.Error, "always", 100] as const,
-		"header-trim": [RuleConfigSeverity.Error, "always"] as const,
-		"subject-case": [
-			RuleConfigSeverity.Error,
-			"never",
-			["sentence-case", "start-case", "pascal-case", "upper-case"],
-		] as [RuleConfigSeverity, RuleConfigCondition, TargetCaseType[]],
-		"subject-empty": [RuleConfigSeverity.Error, "never"] as const,
-		"subject-full-stop": [RuleConfigSeverity.Error, "never", "."] as const,
-		"type-case": [RuleConfigSeverity.Error, "always", "lower-case"] as const,
-		"type-empty": [RuleConfigSeverity.Error, "never"] as const,
-		"type-enum": [
-			RuleConfigSeverity.Error,
-			"always",
-			[
-				"build",
-				"chore",
-				"ci",
-				"docs",
-				"feat",
-				"fix",
-				"perf",
-				"refactor",
-				"revert",
-				"style",
-				"test",
-			],
-		] as [RuleConfigSeverity, RuleConfigCondition, string[]],
-	});
-	if (!result.valid) {
-		const errors = [...result.errors, ...result.warnings]
-			.map((e) => ` - ${e.name}: ${e.message}`)
-			.join("\n");
-		console.error(`Commit message errors:\n${errors}`);
-		console.log();
-		process.exitCode = 1;
-	}
+  const message = fs.readFileSync(process.argv[2]).toString();
+  const result = await lint(message, {
+    'body-leading-blank': [RuleConfigSeverity.Warning, 'always'] as const,
+    'body-max-line-length': [RuleConfigSeverity.Error, 'always', 100] as const,
+    'footer-leading-blank': [RuleConfigSeverity.Warning, 'always'] as const,
+    'footer-max-line-length': [
+      RuleConfigSeverity.Error,
+      'always',
+      100,
+    ] as const,
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 100] as const,
+    'header-trim': [RuleConfigSeverity.Error, 'always'] as const,
+    'subject-case': [
+      RuleConfigSeverity.Error,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ] as [RuleConfigSeverity, RuleConfigCondition, TargetCaseType[]],
+    'subject-empty': [RuleConfigSeverity.Error, 'never'] as const,
+    'subject-full-stop': [RuleConfigSeverity.Error, 'never', '.'] as const,
+    'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'] as const,
+    'type-empty': [RuleConfigSeverity.Error, 'never'] as const,
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ] as [RuleConfigSeverity, RuleConfigCondition, string[]],
+  });
+  if (!result.valid) {
+    const errors = [...result.errors, ...result.warnings]
+      .map((e) => ` - ${e.name}: ${e.message}`)
+      .join('\n');
+    console.log(`Commit message errors:\n${errors}`);
+    process.exitCode = 1;
+  }
 }
 
 main();
